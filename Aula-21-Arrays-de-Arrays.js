c17f7c8cc23f27b3a5b665dbb6bbb615078317ba
Alexandre-Passos <alexandre15passos@gmail.com>
2025-06-14 09:59:12 -0300

    21

---
+++ Aula-21-Arrays-de-Arrays.js
@@ -0,0 +1,7 @@
+const alunos = ['Jão', 'Juliana', 'Caio', 'Ana']
+const medias = [10, 6, 7, 5]
+
+const listaAlunos = [alunos, medias]
+console.log(listaAlunos[0])
+console.log(listaAlunos[1])
+console.log(listaAlunos[0][1])
