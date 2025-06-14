513f0b9056204cc04a2a63ae45943696de97c59e
Alexandre-Passos <alexandre15passos@gmail.com>
2025-06-14 09:51:10 -0300

    Splice

---
+++ Aula-19-Splice.js
@@ -0,0 +1,9 @@
+const listaEstudantes = ['Maria', 'João', 'Lara', 'Lohana', 'John', 'Marcos']
+
+console.log(listaEstudantes.splice(1, 2, 'Rambo'))
+
+console.log(listaEstudantes)
+
+const novaLista = listaEstudantes.splice(1, 2)
+
+console.log(novaLista)
