d7e4b9c7395678c874872a06d0cdd58c50ddfe10
Alexandre-Passos <alexandre15passos@gmail.com>
2025-06-15 10:17:45 -0300

    28

--- Aula-28-Map.js
+++ Aula-28-Map.js
@@ -1,5 +1,5 @@
 const notas = [10, 4.4, 6.7, 8.3, 6.6]
 
-const notasAtualizadasComMap = notas.map(nota => nota + 1)
+const notasAtualizadasComMap = notas.map(nota => nota + 1 >= 10 ? 10 : nota + 1)
 
 console.log(notasAtualizadasComMap)
